4ebfc300c36d22e704de6221d95ff5efae11516e
Anand <anandverma6220@gmail.com>
2021-05-30 09:16:35 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/home_page.dart';
+import 'package:myapp/pages/home_page.dart';
+import 'package:myapp/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final String name = "Anand";
+  final int day = 30;
   @override
   Widget build(BuildContext context) {
-    final String name = "Anand";
-    final int day = 30;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog"),
+        title: Text("Catalog App"),
       ),
       body: Center(
         child: Text("Hi this is $day days of code by $name"),
       ),
+      drawer: Drawer(),
     );
   }
 }
